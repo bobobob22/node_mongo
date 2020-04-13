d2ce3ec3e3c1392bb0c69fa058d44075a2fc753d
Dawid Kwiatoń <dawid.kwiaton@deployed.pl>
2020-04-13 02:54:16 +0200

    add graphql and so on

--- server.js
+++ server.js
@@ -3,7 +3,20 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const DATABASE_URL = 'mongodb://mongo:27017/node'
-const app = express()
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const password = require('passport');
+
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+
+const multer = require('multer');
+const graphqlHttp  = require('express-graphql');
+
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 
 
 mongoose.connect(DATABASE_URL);
@@ -13,11 +26,16 @@ db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
 
+app.use('/graphql', graphqlHttp({
+  schema: graphqlSchema,
+  rootValue: graphqlResolver,
+}));
+
 const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
 
 app.get('/', function(req,res){
-  res.send('helo dawidek)')
+  res.send('helo dawidek123)')
 })
 
 app.listen(3000, () => console.log('Server Started'))
